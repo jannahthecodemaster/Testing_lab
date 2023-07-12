18de620f1474de7ce412140575ecc654ea353fcd
Jannah Anwar <jannah.anwar@outlok.com>
2023-07-12 22:58:00 +0100

    finished MVP

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,19 +1,84 @@
-import java.time.LocalDate;
-
 public class BankAccount {
     private String firstName;
     private String lastName;
     private String dateOfBirth;
 
-    private LocalDate localDate;
+    private String localDate;
     private int accountNumber;
     private int balance;
+    private int overDraft;
 
-    public BankAccount(String firstName) {
+    private double interestRate;
+
+    public BankAccount(String firstName, String lastName, String dateOfBirth, String localDate, int accountNumber, int balance, int overdraft, double interestRate) {
         this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.localDate = localDate;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.overDraft = overdraft;
+        this.interestRate = interestRate;
     }
+
+
     public String getFirstName() {
-        return firstName;
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public String getLocalDate() {
+        return this.localDate;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    //setters
+    public void setFirstName(String name){
+        this.firstName = name;
+    }
+
+    public void setLastName(String name){
+        this.lastName = name;
+    }
+
+    public void setDateOfBirth(String dob){
+        this.dateOfBirth = dob;
+    }
+    public void setAccountNumber(int accountNumber){
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(int balance){
+        this.balance = balance;
+    }
+
+    public int addBalance(int extraBalance) { //ASK WHY WHEN I DO PUBLIC VOID IT SAYS VOID AND INT ARE INCOMPATIBLE
+       return this.balance += extraBalance;
+    }
+
+    public String removeBalance(int balanceToBeTaken) {
+        if (this.balance - balanceToBeTaken > this.overDraft){
+            this.balance -= balanceToBeTaken;
+            return "Completed withdrawal";
+        } else return "You have reached your overdraft limit: incomplete withdrawal";
+    }
+
+    public double payInterest(double interestRate){
+        return this.balance *= interestRate;
     }
 
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.extractProperty;
 
 public class BankAccountTest {
 
@@ -9,7 +10,7 @@ public class BankAccountTest {
 
     @BeforeEach
     public void setUp() {
-        bankaccount = new BankAccount("Jannah");
+        bankaccount = new BankAccount("Jannah", "Anwar", "30/03/2002", "12/07/2023", 12345678, 0, -50, 1.75);
     }
 
     @Test
@@ -21,7 +22,119 @@ public class BankAccountTest {
         String expected = "Jannah";
         assertThat(actual).isEqualTo(expected);
     }
+    @Test
+    public void hasLastName(){
+        //Given
+        //When
+        String actual = bankaccount.getLastName();
+        //Then
+        String expected = "Anwar";
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    public void hasDateOfBirth(){
+        //Given
+        //When
+        String actual = bankaccount.getDateOfBirth();
+        //Then
+        String expected = "30/03/2002";
+        assertThat(actual).isEqualTo(expected);
+    }
 
+    @Test
+    public void hasLocalDate(){
+        //Given
+        //When
+        String actual = bankaccount.getLocalDate();
+        //Then
+        String expected = "12/07/2023";
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    public void getAccountNumberTest(){
+        //Given
+        //When
+        int actual = bankaccount.getAccountNumber();
+        //Then
+        int expected = 12345678;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void getBalanceTest(){
+        //Given
+        //When
+        int actual = bankaccount.getBalance();
+        //Then
+        int expected = 0;
+        assertThat(actual).isEqualTo(expected);
+    }
+//setters
+    @Test
+    public void setFirstName(){
+        bankaccount.setFirstName("Bob");
+        String actual = bankaccount.getFirstName();
+        String expected = "Bob";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public void setLastName(){
+        bankaccount.setFirstName("Marley");
+        String actual = bankaccount.getLastName();
+        String expected = "Marley";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public void setDateOfBirth(){
+        bankaccount.setDateOfBirth("22/08/2002");
+        String actual = bankaccount.getDateOfBirth();
+        String expected = "22/08/2002";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void setAccountNumber(){
+        bankaccount.setAccountNumber(87654321);
+        int actual = bankaccount.getAccountNumber();
+        assertThat(actual).isEqualTo(87654321);
+    }
+
+    @Test
+    public void setBalance() {
+        bankaccount.setBalance(3200);
+        int actual = bankaccount.getBalance();
+        assertThat(actual).isEqualTo(3200);
+    }
+
+    @Test
+    public void deposit(){
+        //Given
+        //When
+        bankaccount.addBalance(300);
+        int actual = bankaccount.getBalance();
+        //Then
+        int expected = 300;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void withdrawal(){
+        //Given
+        //When
+        bankaccount.removeBalance(100);
+        int actual = bankaccount.getBalance();
+        //Then
+        int expected = -100;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void payInterestTest(){
+        bankaccount.payInterest(1.75);
+        double actual = bankaccount.getBalance();
+        double expected = 0 * 1.75;
+        assertThat(actual).isEqualTo(expected);
+    }
 
 }
 
